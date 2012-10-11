cd71b339f07bf7ea138961c0240e89f724b6e0c6
Luis Urraca <l.urraca@gmail.com>
2012-10-11 13:26:29 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HangueoApi::Application.config.secret_token = 'fe1122ad34d87f4bc31c0337b20a71160152f769affc1603c7048796c5b344b5c63fcce13f220fa1da16c53aca28c8231481ff483d0564f24357205bfa14b5c4'
